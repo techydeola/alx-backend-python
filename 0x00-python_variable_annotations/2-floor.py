cb503cc912ac219e33d40b981a5587bbb05604dc
Johnson adeola <johnsamxy@gmail.com>
2024-03-14 16:29:16 +0100

    quick fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import math
-
 """ This is a module of type-annotated function
     floor which takes a float n as argument and returns
     the floor of the float.
 """
+
+import math
 
 
 def floor(n: float) -> int:
