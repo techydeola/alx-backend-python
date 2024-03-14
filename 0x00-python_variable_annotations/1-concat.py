cbf865edec6e1b2090e4506a312a848d683c7546
Johnson adeola <johnsamxy@gmail.com>
2024-03-14 16:09:40 +0100

    new task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" This is a module of a type-annotated function concat
+    that takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes two args(str) and returns a concatenated string
+        Return: str1 + str 2
+    """
+    return str1 + str2
