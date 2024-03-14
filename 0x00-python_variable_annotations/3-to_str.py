fa5b17a02c03d9ca4d12ccc66100e0d08f5ad9ea
Johnson adeola <johnsamxy@gmail.com>
2024-03-14 16:45:36 +0100

    new task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" a type-annotated function to_str that takes a float n as argument
+    and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """ takes an args and return its string representation
+        returns: type str
+    """
+    return str(n)
