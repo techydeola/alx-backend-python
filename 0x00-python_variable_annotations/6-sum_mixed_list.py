6e983232de0bb903e26b1a6800f0209070f80714
Johnson adeola <johnsamxy@gmail.com>
2024-03-14 19:54:24 +0100

    new task

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" This module is a type-annotated function sum_mixed_list which takes
+    a list mxd_lst of integers and floats and returns their sum as a float.
+"""
+
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """ takes an args(list[float or int]) and return sum of all elements
+        returns: type str
+    """
+    total_sum = sum(x for x in mxd_lst)
+
+    return total_sum
