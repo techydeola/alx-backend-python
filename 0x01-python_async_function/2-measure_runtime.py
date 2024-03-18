7abcf4cbfbf4ba8b51b6b6787c515a0708450bf8
Johnson adeola <johnsamxy@gmail.com>
2024-03-18 15:42:17 +0100

    new task

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+    This module makes use of the asynchronous function that is imported from
+    0-basic_async_function and measures the run time
+"""
+
+
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+        An asynchronous function that measure run time for an async function
+        Return: elapsed time / 2
+    """
+    start_time: float = time.time()
+    await wait_n(n, max_delay)
+    end_time: float = time.time()
+    elapsed_time: float = end_time - start_time
+
+    return elapsed_time / 2
