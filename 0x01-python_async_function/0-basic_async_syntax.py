14034e846385f34a08e29664df3516a69504ee8e
Johnson adeola <johnsamxy@gmail.com>
2024-03-18 14:03:21 +0100

    added type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,14 +10,15 @@
 
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
         An asynchronous function that waits for a random seconds and returns it
         Return: random value
     """
 
-    random_delay = random.uniform(0, max_delay)
+    random_delay: Union[float, int] = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
